9a0b7b719ee7894a3ad60397f1384e26a4616550
srikanth velma <srikanthvelma>
2023-03-30 12:51:10 +0530

    containername changed

--- azure/backend_demo/provider.tf
+++ azure/backend_demo/provider.tf
@@ -8,7 +8,7 @@ terraform {
   backend "azurerm" {
     resource_group_name = "terraformbackend"
     storage_account_name = "srterraformbackend"
-    container_name = "tfstate"
+    container_name = "terraformstate"
     key = "j2f8LyRqxeZHOtDu5bItWG+1yYcl1y8uQAUhXZ0w9Nlvuu6ZrtNqTQyW5KbdTOKcGGtK+s4zfjXG+AStorv17w=="
     
   }
